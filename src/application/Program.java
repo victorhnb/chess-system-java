73889e5802c9a1f7c8e88ebf806ee95e6108b411
Victor Hugo <victor_nascimento1996@hotmail.com>
2020-03-30 17:30:07 -0300

    First Class Posistion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Position p1 = new Position(3, 5);
+		System.out.println(p1);
+		
+		
 	}
 
 }
