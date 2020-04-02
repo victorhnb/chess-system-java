1430330e731f18f3274cf8604dd793ae85184031
Victor Hugo <victor_nascimento1996@hotmail.com>
2020-04-02 03:37:50 -0300

    Chess layer and priting the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,16 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board b1 = new Board(8,8);
+
 		
-		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,9 +11,33 @@ public class Board {
 		this.columns = columns;
 		pieces = new Piece[rows] [columns];
 	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
 	
+	public Piece piece(int rows, int columns) {
+		return pieces[rows][columns];
+	}
 	
-	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+				
+			
+				
+	}
 	
 	
 	
